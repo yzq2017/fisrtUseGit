85f5d342d02bf8c8d1e0ea8260c6fb3ec710b031
yzq2017 <328686937@qq.com>
2017-11-12 00:02:03 +0800

    第二次更新代码

---
+++ tomcat102/src/main/java/com/yzq/pojo/People.java
@@ -0,0 +1,29 @@
+package com.yzq.pojo;
+
+public class People {
+	
+	private int  id;
+	
+	private String username;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("111");
+	}
+
+}
